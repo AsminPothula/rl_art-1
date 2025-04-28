cf4dbb76662f1bea5763be269b044560e1a2b880
Keshav Bansal <keshavbansal015@gmail.com>
2025-04-28 11:22:47 -0500

    small updates

--- deep_rl_painter/models/ddpg.py
+++ deep_rl_painter/models/ddpg.py
@@ -98,3 +98,6 @@ class DDPGAgent:
     def soft_update(self, local_model, target_model, tau):
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
+
+
+# UPDATE
